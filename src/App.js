324c7a026da42bd2c71e66b3604227298afce226
RyanLinrm <runminlin@gmail.com>
2019-09-23 13:16:39 -0400

    created baisc App component and init songList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SongList extends React.Component {
+  state = {
+    songList: [
+      {
+        name: "Song1",
+        artist: "Name1",
+        likes: 4,
+        dislikes: 0
+      },
+      {
+        name: "Song2",
+        artist: "Name2",
+        likes: 3,
+        dislikes: 1
+      },
+      {
+        name: "Song3",
+        artist: "Name3",
+        likes: 2,
+        dislikes: 1
+      },
+      {
+        name: "Song4",
+        artist: "Name4",
+        likes: 1,
+        dislikes: 4
+      }
+    ]
+  }
+
+  render(){
+
+    return(
+      <div>
+        <p>I'm rendering</p>
+      </div>
+    )
+  }
+}
+
+
+class App extends React.Component  {
+  
+  render(){
+    return (
+      <div className="App">
+          <input type="text" style={{width:"25%", height:"25px"}} placeholder="Search..."/>
+          <br/>
+          <br/>
+          <div>Now Playing:
+            <br/><br/>
+            <SongList /></div>
+      </div>
+    );
+  }
 }
 
 export default App;
