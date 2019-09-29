a2ab83b9b93e4d9bf4f766770ba1840810ee9af4
Marco <43689503+ValeroM@users.noreply.github.com>
2019-09-29 13:03:03 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser"); // whenever we require a whole directory, node looks for the "index.js" fle FIRST under that directory we required
-const attachRoutes = require("./routes");
+const bodyParser = require("body-parser"); 
+const attachRoutes = require("./routes"); // whenever we require a whole directory, node looks for the "index.js" fle FIRST under that directory we required
 
 app.use(bodyParser.json()); //  basically tells the system that we want json to be used.
 
