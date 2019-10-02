5e6f90fc906414044663b9f0ba806875cf96d42d
RyanLinrm <runminlin@gmail.com>
2019-10-02 00:41:09 -0400

    Added logo and background img, and styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import '../App.css';
+import logo from '../img/logo.png';
 
 export default class HomePage extends React.Component {
     render(){
         return (
-          <div className="App">
-            <h1>WeDJ!</h1>
-            <h2>Some cool and dope lines here</h2>
-            <button className="btn btn-outline-danger">Party Up</button>
-            <br/>
-            <button className="btn btn-outline-success">Guest Login</button>
-            <br/>
-            <button className="btn btn-outline-info">About Us</button>
+          <div className="App App-bg">
+              <img src={logo} alt="logo"></img>
+              <br/>
+              <br/>
+              <h1 className="App-header">Rock your party your way!</h1>
+              <br/>
+              <br/>
+              <Link to="/admin">
+                <button className="btn btn-danger">Party Up</button>
+              </Link>
+              <br/>
+              <Link to="/guest">
+                <button className="btn btn-success">Guest Login</button>
+              </Link>
+              <br/>
+              <button className="btn btn-info">About Us</button>
+              <br/><br/><br/><br/><br/>
           </div>
         );
       }
