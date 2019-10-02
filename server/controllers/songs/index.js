fc5b8c23b2aafae07e719ca392622f83933d86b1
Marco Valero <marcovalero6@gmail.com>
2019-10-02 13:12:02 -0400

    Added a json mock data file for songs. In addition, I added two routes post and get to /songs where you can get songs from the json file or add more songs to it

--- server/controllers/songs/index.js
+++ server/controllers/songs/index.js
@@ -1,8 +1,10 @@
 function setup() {
   return {
     songsOnQueue: require("./songs")(),
-    addSongsToQueue: require("./postsongs")()
+    addSongsToQueue: require("./postsongs")() // this "()" is needed because under the files songs.js and postsongs.js we have a method so its like we are calling a function
   };
 }
 
 module.exports = setup;
+
+// when we import the songs directory, node looks for index.js first (this one), so back under routes/get-songs we are calling the specified controllers
