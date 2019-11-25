b49c11e43c6cb3d7b809ad77f99843f6dbcd6e60
Marco Valero <marcovalero6@gmail.com>
2019-11-25 14:08:19 -0500

    created endpoint to delete a song from queue for a specific lobby

--- server/routes/get-songs.js
+++ server/routes/get-songs.js
@@ -9,7 +9,8 @@ const setup = () => {
   router.get("/", controller.songsOnTableSongs); // calls the actual logic from the controller/songs/songs.js file
   router.post("/", controller.addSongsToTableSongs); // logic from controller/songs/postsongs.js
   router.post("/add", controller.addSongsToQueue); // logic from controller/songs/posttoqueue.js
-  router.get("/queue", controller.getSongsInQueue); // logic from controller/songs/
+  router.get("/queue", controller.getSongsInQueue); // logic from controller/songs/getsongsinqueue.js
+  router.delete("/delete", controller.delSongInQueue); // logic from controller/songs/deletesonginqueue.js
 
   return router;
 };
